e86488fc644963e8d6acd1d53583d3974dddff8b
hitesh-tarani <tarani.hitesh@gmail.com>
2016-05-25 05:03:59 +0530

    modified main for handling exception

--- src/main.java
+++ src/main.java
@@ -9,11 +9,13 @@ public class main
 {
     public static ArrayList<String> crawledUrls = new ArrayList<>();
 
+    public static String baseCrawlUrl = "http://www.mit.edu"; //http://www.insite.iitmandi.ac.in";
+
     public static void main(String[] args) throws IOException
     {
         System.setProperty("http.proxyHost","10.8.0.1");
         System.setProperty("http.proxyPort","8080");
-        processPage("http://www.mit.edu");
+        processPage(baseCrawlUrl);
     }
 
     public static void processPage(String URL) throws IOException
@@ -21,21 +23,28 @@ public class main
         if(!crawledUrls.contains(URL))
         {
             crawledUrls.add(URL);
+            Document doc;
             //get useful information
-            Document doc = Jsoup.connect("http://www.mit.edu/").get();
+            try {
+                doc = Jsoup.connect(URL).get();
 
-            if(doc.text().contains("research"))
-            {
-                System.out.println(URL);
-            }
+                //if(doc.text().contains("research"))
+                {
+                    System.out.println(URL);
+                }
 
-            //get all links and recursively call the processPage method
-            Elements questions = doc.select("a[href]");
-            for(Element link: questions)
-            {
-                if(link.attr("href").contains("mit.edu"))
+                //get all links and recursively call the processPage method
+                Elements questions = doc.select("a[href]");
+                for(Element link: questions)
+                {
+                    //if(link.attr("href").contains("mit.edu"))
                     processPage(link.attr("abs:href"));
+                }
+            } catch (java.net.MalformedURLException e) {
+                System.out.println("Error in this url: " + URL);
             }
+
+
         }
     }
 }
