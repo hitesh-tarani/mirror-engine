ad7550ea31cb84648f02c3b6e9627365b96788b8
Ayush Garg <ayushgarg123@outlook.com>
2016-05-25 10:16:20 +0530

    crawled only http and https links

--- src/main.java
+++ src/main.java
@@ -5,11 +5,12 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class main
 {
     public static ArrayList<String> crawledUrls = new ArrayList<>();
 
-    public static String baseCrawlUrl = "http://www.mit.edu"; //http://www.insite.iitmandi.ac.in";
+    public static String baseCrawlUrl = "http://www.insite.iitmandi.ac.in/insite_wp";
 
     public static void main(String[] args) throws IOException
     {
@@ -20,31 +21,35 @@ public class main
 
     public static void processPage(String URL) throws IOException
     {
-        if(!crawledUrls.contains(URL))
+        if(URL.startsWith("http")||URL.startsWith("https"))
         {
-            crawledUrls.add(URL);
-            Document doc;
-            //get useful information
-            try {
-                doc = Jsoup.connect(URL).get();
-
-                //if(doc.text().contains("research"))
+            if(!crawledUrls.contains(URL))
+            {
+                crawledUrls.add(URL);
+                Document doc;
+                //get useful information
+                try
                 {
-                    System.out.println(URL);
-                }
+                    doc = Jsoup.connect(URL).timeout(10000).get();
 
-                //get all links and recursively call the processPage method
-                Elements questions = doc.select("a[href]");
-                for(Element link: questions)
-                {
-                    //if(link.attr("href").contains("mit.edu"))
-                    processPage(link.attr("abs:href"));
+                    //if(doc.text().contains("research"))
+                    {
+                        System.out.println(URL);
+                    }
+
+                    //get all links and recursively call the processPage method
+                    Elements questions = doc.select("a[href]");
+                    for(Element link: questions)
+                    {
+                        //if(link.attr("href").contains("mit.edu"))
+                        processPage(link.attr("abs:href"));
+                    }
                 }
-            } catch (java.net.MalformedURLException e) {
-                System.out.println("Error in this url: " + URL);
+                catch (Exception e)
+                {
+                    System.out.println("Error in this url: " + URL);
+                }
             }
-
-
         }
     }
 }
