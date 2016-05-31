6a50f86d7139c14f5b7993286ff7ab5db6233af1
hitesh-tarani <tarani.hitesh@gmail.com>
2016-05-31 13:04:32 +0530

    modified for blocking queues in crawlconfig

--- src/crawlConfig.java
+++ src/crawlConfig.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by Hitesh on 24-May-16.
@@ -7,6 +11,10 @@ public class crawlConfig {
     private int maxnumPages;
 
     private File crawlStorageDir;
+
+    private BlockingQueue<url> urlsToCrawl = new LinkedBlockingQueue<>();
+
+    private Set<url> crawledUrls = new HashSet<>(50);
 
     public File getCrawlStorageDir() {
         return crawlStorageDir;
